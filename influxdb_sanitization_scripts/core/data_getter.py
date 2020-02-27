5a2d8793d9698f1c6ea39c90a7f2623a66dd2b85
zom <t.fontana@net2action.com>
2020-02-27 11:26:52 +0100

    addes strim instead of trim

--- influxdb_sanitization_scripts/core/data_getter.py
+++ influxdb_sanitization_scripts/core/data_getter.py
@@ -66,6 +66,6 @@ class DataGetter:
     def get_tag_values(self, tag):
         result = self.exec_query("""SHOW TAG VALUES WITH KEY = "{tag}" """.format(tag=tag))
         return [
-            x["value"].trim("'")
+            x["value"].strip("'")
             for x in result
         ]
