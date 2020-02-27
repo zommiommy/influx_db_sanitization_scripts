e2666439b5e57831193efec324aa9e89ee0d814b
zom <t.fontana@net2action.com>
2020-02-27 18:06:25 +0100

    changed epoch to ns_epoch in time chunker

--- influxdb_sanitization_scripts/core/time_chunks.py
+++ influxdb_sanitization_scripts/core/time_chunks.py
@@ -2,14 +2,15 @@
 from .parse_time import parse_time
 
 def time_chunks(start, delta, chunk):
+    k = 1_000_000
     time = parse_time(delta)
     chunk = parse_time(chunk)
     low = 0
     high = min(chunk, time)
 
-    yield int(start - low), int(start - high)
+    yield int(start - low) * k, int(start - high) * k
 
     while high < time:
         low = high
         high = min(low + chunk, time)
-        yield int(start - low), int(start - high)
+        yield int(start - low) * k, int(start - high) * k
