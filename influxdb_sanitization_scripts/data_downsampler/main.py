f473d4f9e808056e85222bfeaa2c6fcfe06aea26
Tommaso Fontana <tommaso.fontana.96@gmail.com>
2020-03-06 12:29:04 +0100

    fix

--- influxdb_sanitization_scripts/data_downsampler/main.py
+++ influxdb_sanitization_scripts/data_downsampler/main.py
@@ -19,7 +19,7 @@ def data_downsampler(data_getter: DataGetter, measurement: str, window: str="10m
 
         data = data_getter.exec_query(AGGREGATE.format(**locals()))
         df = pd.DataFrame(data)
-        index = pd.PeriodIndex(second=df.time)
+        index = pd.PeriodIndex(second=df.time.astype(str))
         df.set_index('time')
         df["hostname"] = hostname
         df["service"]  = service
