1f064311bab71639baff259609bfe33a837de403
Tommaso Fontana <tommaso.fontana.96@gmail.com>
2020-03-06 12:31:14 +0100

    fix

--- influxdb_sanitization_scripts/data_downsampler/main.py
+++ influxdb_sanitization_scripts/data_downsampler/main.py
@@ -20,7 +20,7 @@ def data_downsampler(data_getter: DataGetter, measurement: str, window: str="10m
         data = data_getter.exec_query(AGGREGATE.format(**locals()))
         df = pd.DataFrame(data)
         index = pd.to_datetime(df.time, unit="s")
-        df.set_index('time')
+        df.set_index(index)
         df["hostname"] = hostname
         df["service"]  = service
         df["metric"]   = metric
