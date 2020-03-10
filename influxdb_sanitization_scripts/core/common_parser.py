f66d7a9c8fdebf1fe45b4472bd947891294d7294
Tommaso Fontana <tommaso.fontana.96@gmail.com>
2020-03-10 17:53:02 +0100

    fixed examples format

--- influxdb_sanitization_scripts/core/common_parser.py
+++ influxdb_sanitization_scripts/core/common_parser.py
@@ -1,5 +1,6 @@
 import sys
 import argparse
+from argparse import RawTextHelpFormatter
 
 class MyParser(argparse.ArgumentParser):
     """Custom parser to ensure that the exit code on error is 1
@@ -13,7 +14,7 @@ class MyParser(argparse.ArgumentParser):
 
         
 def get_common_parser(description):
-    parser = MyParser(description=description)
+    parser = MyParser(description=description, formatter_class=RawTextHelpFormatter)
     parser.add_argument("-v", "--verbosity", type=int, help="Verbosity of the program. 0 - Critical, 1 - Warnings, 2 - Info, 3 - Debug", default=0)
     parser.add_argument("-dr", "--dryrun", default=False, action="store_true", help="Test run, access the DB in read only mode.")
     parser.add_argument("-f", "--force", default=False, action="store_true", help="No not show the are you sure prompt.")
